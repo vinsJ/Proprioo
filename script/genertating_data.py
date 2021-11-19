a5533a16d043b617445ec2cd9e495dad5c7666dc
vinsJ <45200439+vinsJ@users.noreply.github.com>
2021-11-19 18:21:23 +0100

    :memo: add docstring + fromatting

--- script/genertating_data.py
+++ script/genertating_data.py
@@ -1,4 +1,4 @@
-# This file must be run one time. 
+# This file must be run one time.
 
 
 import json
@@ -7,25 +7,25 @@ from random import randint
 
 from models.properties import Properties
 
+
 def generate_data():
     """ Method to generate data property 
-    
+
     Generate data and save it into JSON file
     """
 
     list_properties = []
 
-    for i in range(0,100):
+    for i in range(0, 100):
 
-            surface = randint(70, 120)
-            price = randint(450000, 800000)
+        surface = randint(70, 120)
+        price = randint(450000, 800000)
 
-            p = Properties(i, 'house', 5, surface, price, True, 0)
-            list_properties.append(p.__dict__)
-
-    print(list_properties)
+        p = Properties(i, 'house', 5, surface, price, True, 0)
+        list_properties.append(p.__dict__)
 
     with open('./data/generated-property.json', 'w', encoding='utf-8') as f:
         json.dump(list_properties, f)
+
 
 generate_data()
